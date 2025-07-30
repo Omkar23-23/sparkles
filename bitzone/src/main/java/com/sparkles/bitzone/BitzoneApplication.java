28907f55c0c1991b43c31bef564b4d4540083247
Omkar23-23 <omkarbangar77@gmail.com>
2025-07-30 12:58:34 +0530

    Initial commit

---
+++ bitzone/src/main/java/com/sparkles/bitzone/BitzoneApplication.java
@@ -0,0 +1,13 @@
+package com.sparkles.bitzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitzoneApplication.class, args);
+	}
+
+}
---
+++ bitzone/src/test/java/com/sparkles/bitzone/BitzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkles.bitzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
